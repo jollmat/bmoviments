e92c33e2a3b73d81d2b00e17b0e5d764d2856d09
Joan <jollmat@hotmail.com>
2022-01-19 14:45:36 +0100

    Environment fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDdTdVYOuw3dbfb2nLaSseDDpmq7Tw4Bsc",
+    authDomain: "bs-moviments.firebaseapp.com",
+    databaseURL: "https://bs-moviments.firebaseio.com",
+    projectId: "bs-moviments",
+    storageBucket: "bs-moviments.appspot.com",
+    messagingSenderId: "271178798025",
+    appId: "1:271178798025:web:0007d0e5eea6539d358e18"
+  }
 };
